aae03cbffdd182cec07b62846366bebc39f0b955
Akira Hayakawa <ruby.wktk@gmail.com>
2022-02-24 20:31:06 +0900

    Use linear scan in add/remove node

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,17 @@
 //! ```
 //! use asura::*;
 //!
-//! let mut builder = SegmentTableBuilder::new();
-//! builder.add_segment(0, 10.);
-//! builder.add_segment(1, 5.);
-//! builder.add_segment(2, 8.);
+//! let mut seg = SegmentTable::new();
+//! seg.add_node(0, 10.);
+//! seg.add_node(1, 5.);
+//! seg.add_node(2, 8.);
+//! seg.remove_node(1);
 //!
-//! let seg = builder.build();
 //! let searcher = Searcher::new(&seg);
 //! let data_key = 43287642786;
 //! let assign_node_id = searcher.search(data_key);
-//! assert!(assign_node_id < 2);
+//! assert!(assign_node_id < 3);
+//! assert_ne!(assign_node_id, 1);
 //! ```
 
 use std::collections::HashMap;
@@ -23,27 +24,67 @@ mod rand;
 
 pub type NodeId = u64;
 
-pub struct Segment {
+struct Segment {
     node_id: NodeId,
     len: f64,
 }
-impl Segment {
-    pub fn new(node_id: NodeId, len: f64) -> Self {
-        assert!(len > 0.);
-        assert!(len <= 1.);
-        Self { node_id, len }
-    }
-}
 
 pub struct SegmentTable {
-    /// Set of segments.
-    pub h: HashMap<u64, Segment>,
-    /// Max bound of all the segments.
-    pub max_bound: f64,
+    h: HashMap<u64, Segment>,
+    max_bound: f64,
 }
 impl SegmentTable {
+    /// Create an empty segment table.
+    pub fn new() -> Self {
+        Self {
+            h: HashMap::new(),
+            max_bound: 0.,
+        }
+    }
     fn is_empty(&self) -> bool {
         self.h.is_empty()
+    }
+    fn recalc_max_bound(&mut self) {
+        let mut maxv = 0.;
+        for (l, seg) in &self.h {
+            let r = *l as f64 + seg.len;
+            if r > maxv {
+                maxv = r;
+            }
+        }
+        self.max_bound = maxv;
+    }
+    /// Add a node with capacity.
+    /// Cost: O(n) where n is the size of this segment table.
+    pub fn add_node(&mut self, node_id: NodeId, cap: f64) {
+        let mut remaining = cap;
+        for l in 0.. {
+            if remaining == 0. {
+                break;
+            }
+            let vacant = !self.h.contains_key(&l);
+            if vacant {
+                let len = if remaining > 1. { 1. } else { remaining };
+                remaining -= len;
+                let seg = Segment { node_id, len };
+                self.h.insert(l, seg);
+            }
+        }
+        self.recalc_max_bound()
+    }
+    /// Remove a node.
+    /// Cost: O(n) where n is the size of this segment table.
+    pub fn remove_node(&mut self, node_id: NodeId) {
+        let mut removes = vec![];
+        for (l, seg) in &self.h {
+            if seg.node_id == node_id {
+                removes.push(*l);
+            }
+        }
+        for l in removes {
+            self.h.remove(&l);
+        }
+        self.recalc_max_bound()
     }
     fn search_once(&self, x: f64) -> Option<NodeId> {
         let k = f64::floor(x) as u64;
@@ -61,48 +102,6 @@ impl SegmentTable {
                     }
                 }
             }
-        }
-    }
-}
-
-pub struct SegmentTableBuilder {
-    h: HashMap<u64, Segment>,
-    next: u64,
-}
-impl SegmentTableBuilder {
-    pub fn new() -> Self {
-        Self {
-            h: HashMap::new(),
-            next: 0,
-        }
-    }
-
-    pub fn add_segment(&mut self, node_id: NodeId, len: f64) {
-        let mut remaining = len;
-        while remaining > 0. {
-            let len = if remaining >= 1. {
-                remaining -= 1.;
-                1.
-            } else {
-                let tmp = remaining;
-                remaining = 0.;
-                tmp
-            };
-            self.h.insert(
-                self.next,
-                Segment {
-                    node_id: node_id,
-                    len: len,
-                },
-            );
-            self.next += 1;
-        }
-    }
-
-    pub fn build(self) -> SegmentTable {
-        SegmentTable {
-            h: self.h,
-            max_bound: self.next as f64,
         }
     }
 }
